69055d06f6576e823b8474be27ebc94b5bbd26d1
Dennis Chatkhan <mealthebear@gmail.com>
2020-06-26 10:17:07 -0700

    Removed unnecessary elements

--- client/src/components/TaskBoard.js
+++ client/src/components/TaskBoard.js
@@ -5,27 +5,19 @@ const TaskBoard = (props) => (
     <div className="task-board">
         <div className="status-block">
             <div className="status-title">To Do</div>
-            <div className="tasks">
                 <TaskCard />
-            </div>
         </div>
         <div className="status-block">
             <div className="status-title">In Progress</div>
-            <div className="tasks">
                 <TaskCard />
-            </div>
         </div>
         <div className="status-block">
             <div className="status-title">Ready for Testing</div>
-            <div className="tasks">
                 <TaskCard />
-            </div>
         </div>
         <div className="status-block">
             <div className="status-title">Done</div>
-            <div className="tasks">
                 <TaskCard />
-            </div>
         </div>
     </div>
 )
