8cee0d434607cce9896df1ac9c2cc464f6d6f53b
Dennis Chatkhan <mealthebear@gmail.com>
2020-06-25 22:40:51 -0700

    Added assignee name to task cards

--- client/src/components/TaskBoard.js
+++ client/src/components/TaskBoard.js
@@ -5,25 +5,25 @@ const TaskBoard = (props) => (
         <div className="status-block">
             <div className="status-title">To Do</div>
             <div className="tasks">
-                <div>Beat Mark Zuckerberg in a Coding Challenge</div>
+                <div>Beat Mark Zuckerberg in a Coding Challenge (Assigned to John Doe)</div>
             </div>
         </div>
         <div className="status-block">
             <div className="status-title">In Progress</div>
             <div className="tasks">
-                <div>Get good at React</div>
+                <div>Get good at React (Assigned to John Doe)</div>
             </div>
         </div>
         <div className="status-block">
             <div className="status-title">Ready for Testing</div>
             <div className="tasks">
-                <div>Banana-inator 3000 Prototype</div>
+                <div>Banana-inator 3000 Prototype (Assigned to John Doe)</div>
             </div>
         </div>
         <div className="status-block">
             <div className="status-title">Done</div>
             <div className="tasks">
-                <div>Watch Cat Videos on YouTube</div>
+                <div>Watch Cat Videos on YouTube (Assigned to John Doe)</div>
             </div>
         </div>
     </div>
