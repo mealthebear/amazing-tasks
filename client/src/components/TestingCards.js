81ced555759e0a20e4be4a2463d2034cc44b81fa
Dennis Chatkhan <mealthebear@gmail.com>
2020-06-26 15:14:08 -0700

    Added key to each rendered list item

--- client/src/components/TestingCards.js
+++ client/src/components/TestingCards.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TestingCards = (props) => (
     props.testing.map((card, id) => {
-        return <div className="task">
+        return <div className="task" key={id}>
                     <p>{card.title}</p>
                     <p className="task-type">{card.type}</p><p className="assignee">{card.assignee}</p>
         </div>
