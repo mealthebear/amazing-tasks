c9c774a0a42d61b1ba9c4235dc6453917265f629
Dennis Chatkhan <mealthebear@gmail.com>
2020-06-26 14:29:03 -0700

    Refactored TaskBoard component to be stateful

--- client/src/components/TaskBoard.js
+++ client/src/components/TaskBoard.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TaskCard from './TaskCard.js';
 
-const TaskBoard = (props) => (
-    <div className="task-board">
-        <div className="status-block status-block-start">
-            <div className="status-title">To Do</div>
-                <TaskCard />
-        </div>
-        <div className="status-block">
-            <div className="status-title">In Progress</div>
-                <TaskCard />
-        </div>
-        <div className="status-block">
-            <div className="status-title">Ready for Testing</div>
-                <TaskCard />
-        </div>
-        <div className="status-block status-block-end">
-            <div className="status-title">Done</div>
-                <TaskCard />
-        </div>
-    </div>
-)
+let exampleCard = {
+    title: 'Fix a small bug',
+    type: 'Bug',
+    assignee: 'John Doe'
+};
+
+class TaskBoard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todo: [exampleCard],
+            inProgress: [exampleCard],
+            testing: [exampleCard],
+            done: [exampleCard],
+        };
+    }
+
+    render() {
+        return (
+            <div className="task-board">
+                <div className="status-block status-block-start">
+                    <div className="status-title">To Do</div>
+                        <TaskCard />
+                </div>
+                <div className="status-block">
+                    <div className="status-title">In Progress</div>
+                        <TaskCard />
+                </div>
+                <div className="status-block">
+                    <div className="status-title">Ready for Testing</div>
+                        <TaskCard />
+                </div>
+                <div className="status-block status-block-end">
+                    <div className="status-title">Done</div>
+                        <TaskCard />
+                </div>
+            </div>
+        )
+    }
+}
 
 export default TaskBoard;
