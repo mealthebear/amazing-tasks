016c99f80338ee7de478b49104f5a13abfdaf697
Dennis Chatkhan <mealthebear@gmail.com>
2020-06-27 18:22:13 -0700

    Constructed method to sort tasks based on status

--- client/src/components/TaskBoard.js
+++ client/src/components/TaskBoard.js
@@ -6,42 +6,72 @@ import TestingCards from './TestingCards.js';
 import TodoCards from './TodoCards.js';
 
 let exampleCard = {
+  assignee: 'John Doe',
+  status: 'todo',
   title: 'Fix a small bug',
   type: 'Bug',
-  assignee: 'John Doe',
 };
 
 class TaskBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todo: [exampleCard],
-      inProgress: [exampleCard],
-      testing: [exampleCard],
-      done: [exampleCard],
+      allTasks: [exampleCard],
+      done: [],
+      progress: [],
+      test: [],
+      todo: [],
     };
-}
+    this.sortTasks = this.sortTasks.bind(this);
+  }
 
-    render() {
-      return (
-        <div className="task-board">
-          <div className="status-block status-block-start">
-            <div className="status-title">To Do</div>
-              <TodoCards todo={this.state.todo} />
-          </div>
-          <div className="status-block">
-            <div className="status-title">In Progress</div>
-              <InProgressCards inProgress={this.state.inProgress} />
-          </div>
-          <div className="status-block">
-            <div className="status-title">Ready for Testing</div>
-              <TestingCards testing={this.state.testing} />
-          </div>
-          <div className="status-block status-block-end">
-            <div className="status-title">Done</div>
-              <DoneCards done={this.state.done} />
-          </div>
+  sortTasks() {
+    let doneTasks = [];
+    let progressTasks = [];
+    let testTasks = [];
+    let todoTasks = [];
+
+    for (let iterator = 0; iterator < this.state.allTasks.length; iterator++) {
+      let currentTask = this.state.allTasks[iterator];
+      if (currentTask.status === 'done') {
+        doneTasks.push(currentTask);
+      } else if (currentTask.status === 'progress') {
+        progressTasks.push(currentTask);
+      } else if (currentTask.status === 'test') {
+        testTasks.push(currentTask);
+      } else {
+        todoTasks.push(currentTask);
+      }
+    }
+
+    this.setState({
+      done: doneTasks,
+      progress: progressTasks,
+      test: testTasks,
+      todo: todoTasks,
+    })
+  }
+
+  render() {
+    return (
+      <div className="task-board">
+        <div className="status-block status-block-start">
+          <div className="status-title" onClick={this.sortTasks}>To Do</div>
+            <TodoCards todo={this.state.todo} />
         </div>
+        <div className="status-block">
+          <div className="status-title">In Progress</div>
+            <InProgressCards progress={this.state.progress} />
+        </div>
+        <div className="status-block">
+          <div className="status-title">Ready for Testing</div>
+            <TestingCards test={this.state.test} />
+        </div>
+        <div className="status-block status-block-end">
+          <div className="status-title">Done</div>
+            <DoneCards done={this.state.done} />
+        </div>
+      </div>
     )
   }
 }
