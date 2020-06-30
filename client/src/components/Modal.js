c6dcf5e18578eae399c25e17e9cc7213cb0e67f8
Dennis Chatkhan <mealthebear@gmail.com>
2020-06-30 09:35:06 -0700

    Outlined the form and inputs the modal will use

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -10,6 +10,43 @@ class Modal extends Component {
     return (
       <div id="task-modal">
         This is the Modal woohooo!
+        <form>
+          <label>Task Name</label>
+          <input type="text"/>
+
+          <div>
+            <label>Type</label>
+            <select defaultValue="Task" onChange={(e) => console.log(e.target.value)}>
+              <option>Task</option>
+              <option>Bug</option>
+              <option>New Feature</option>
+            </select>
+          </div>
+
+          <div>
+            <label>Status</label>
+            <select defaultValue="To Do" onChange={(e) => console.log(e.target.value)}>
+              <option>To Do</option>
+              <option>In Progress</option>
+              <option>Ready for Testing</option>
+              <option>Done</option>
+            </select>
+          </div>
+
+          <div>
+            <label>Assignee</label>
+            <select defaultValue="Jane Doe" onChange={(e) => console.log(e.target.value)}>
+              <option>Choose here</option>
+              <option>Jane Doe</option>
+              <option>John Doe</option>
+              <option>Joe Shmoe</option>
+              <option>Josephine Smith</option>
+            </select>
+          </div>
+
+          <button type="submit" value="update">Update</button>
+
+        </form>
       </div>
     )
   }
